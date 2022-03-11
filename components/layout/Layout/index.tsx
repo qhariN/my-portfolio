c6a869c63e56cb1ed3dba2c4d679b958614e9af0
Jhorman Rus <jhormanrus@gmail.com>
2022-03-11 18:36:57 -0500

    Create navbar

--- components/layout/Layout/index.tsx
+++ components/layout/Layout/index.tsx
@@ -4,7 +4,7 @@ export default function PageLayout ({ children }: { children: React.ReactNode })
   return (
     <>
       <Navbar />
-      <main>
+      <main className="max-w-screen-lg mx-auto p-2 sm:px-6 sm:py-3 lg:px-8 lg:py-4">
         {children}
       </main>
     </>
---
+++ pages/projects.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Projects: NextPage = () => {
+  return (
+    <>
+      projects
+    </>
+  )
+}
+
+export default Projects
