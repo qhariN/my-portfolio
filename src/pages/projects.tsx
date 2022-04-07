fdc7310d496a5add18bc7cd2f52a740dca82b259
Jhorman Rus <jhormanrus@gmail.com>
2022-04-07 16:44:13 -0500

    Fix font letter spacing

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -21,7 +21,7 @@ const Projects: NextPage = () => {
         <p>Some of my public projects. View them all on GitHub.</p>
       </div>
       <div className='grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4 mt-4'>
-        {projects.map((project, index) => (
+        {projects.filter(project => project.visible).map((project, index) => (
           <CardProject key={index} project={project} priority={index < 6? true : false} />
         ))}
       </div>
