45e96a0052cc5922fa9b399b8d3bb88b45dc0819
Jhorman Rus <jhormanrus@gmail.com>
2022-04-07 16:47:28 -0500

    Fix main display and padding

--- src/components/layout/Layout/index.tsx
+++ src/components/layout/Layout/index.tsx
@@ -5,7 +5,7 @@ export default function PageLayout ({ children }: { children: React.ReactNode })
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="max-w-screen-lg mx-auto w-full flex-grow p-2 sm:px-6 sm:py-3 lg:px-8 lg:py-4">
+      <main className="max-w-screen-lg mx-auto w-full flex flex-col flex-grow px-2 py-4 sm:px-6 sm:py-5 lg:px-8 lg:py-6">
         {children}
       </main>
       <Footer />
