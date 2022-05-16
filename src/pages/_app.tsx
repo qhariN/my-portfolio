ce87b95f382aa6e6ef5647b286dc8b1dd00e2892
Jhorman Rus <jhormanrus@gmail.com>
2022-05-16 18:57:16 -0500

    feat: opengraph tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="Web Developer. Passionate about software development and I like to learn new things, solve problems and team work." />
         <meta name="author" content="Jhorman Ruswel Tito Tito" />
         <meta name="robots" content="index,follow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Jhorman Ruswel Tito Tito" />
+        <meta property="og:site_name" content="Jhorman Ruswel Tito Tito" />
+        <meta property="og:description" content="Web Developer. Passionate about software development." />
+        <meta property="og:url" content="https://jhormanrus.vercel.app/" />
+        <meta property="og:image" content="https://res.cloudinary.com/jhormanrus/image/upload/v1652742388/banner-dark.jpg" />
+        <meta property="og:image:alt" content="photo of the author" />
+        <meta property="og:locale" content="en" />
       </Head>
       <Layout>
         <Component {...pageProps} />
