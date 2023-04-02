e4c9b718740f050392c5d7a1555da0af53138353
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-04-02 23:22:23 +0000

    custom colors theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx}'],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000000',
+      white: '#FFFFFF',
+      coral: '#FF7F50',
+      neutral: '#404040',
+      gray: '#9CA3AF',
+      navy: '#00ABB3',
+      yellow: '#FFD700',
+      zinc: '#18181B'
+    }
   },
   plugins: [],
   darkMode: 'class'
