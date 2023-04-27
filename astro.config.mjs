3e77c0ccf1a7038c2dde94cb9b36e973b7637678
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-04-26 22:40:45 -0500

    Revert "add mdx"
    This reverts commit d5a721b63d85e8e43544b60decf96a6b6126c7a6.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config'
+
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import astroI18next from 'astro-i18next'
-import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), astroI18next(), mdx()]
+  integrations: [react(), tailwind(), astroI18next()]
 })
