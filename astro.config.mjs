9562c98a66a12bf1c0ee6da530cb4b15d0acd0e4
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-05-05 14:35:12 -0500

    update npm dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), astroI18next()]
+  integrations: [tailwind(), astroI18next()]
 })
