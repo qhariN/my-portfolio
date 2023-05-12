e7b57918cc679c1af629e421d0c28a09e08e015f
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-05-12 18:28:19 -0500

    start migration from tailwind to master

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroI18next()]
+  integrations: [astroI18next()]
 })
